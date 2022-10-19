c2b14ec56b4362e8431bbf744bb838aed6dca4a9
Samir karmacharya <samirkarmacharya68@gmail.com>
2022-10-19 15:01:50 +0545

    Remove unuse function
    Remove unuse function and comments

--- src/main.js
+++ src/main.js
@@ -1,57 +1,5 @@
 import $ from 'jquery';
 import { useSelect } from '@wordpress/data';
-/*import { useBlockProps } from '@wordpress/block-editor';*/
-/*
-const allPosts = useSelect((select) => {
-	return wp.data.select('core/block-editor').getBlockAttributes('create-block/agutenblog').attributes;
-});
-
-console.log(allPosts);
-debugger;*/
-$(function ($) {
-	/* $('body').on('click', 'a.page-numbers', function (e){
-        e.preventDefault();
-        console.log(myAjax.ajaxurl);
-        $.ajax({
-            type: "post",
-            dataType: "json",
-            url: '/getPosts/v1/getHTML',
-            data: {
-                action: "load_ajax_archive_list",
-                /!* current_category_id: current_category_id,
-                 feature_post_ids: feature_post_ids,
-                 per_page: per_page,
-                 pageNumber: pageNumber*!/
-            },
-            beforeSend: function() {
-                /!*loadMoreButton.addClass('loading').html('+ Load More...');
-                targetSection.addClass('loading-section');*!/
-            },
-            success: function(response) {
-                /!*loadMoreButton.attr('data-pageNumber', (pageNumber + 1));
-                loadMoreButton.closest('div').before(response);
-
-                if (response === '') {
-                    loadMoreButton.hide();
-                } else {
-                    loadMoreButton.show();
-                }
-    *!/
-                $('.list-items').append(response.data.data);
-                console.log(response);
-            },
-            complete: function() {
-                /!* loadMoreButton.removeClass('loading').html('+ Load More');
-                 targetSection.removeClass('loading-section');*!/
-            },
-            error: function(xhr) {
-                // if error occured
-                alert("Error occured. Please try again");
-                console.log(xhr.statusText + xhr.responseText);
-            },
-        });
-    });*/
-});
 
 function getResults() {
 	$.when($.getJSON('http://localhost/wordpress/gutenberg/wordpress/wp-json/getPosts/v1/getHTML?id=63')).then(
